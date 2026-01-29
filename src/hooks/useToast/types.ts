a515e07738be2bff6912eb993441c3232d734b50
IgorSasaki <igor-sasaki@hotmail.com>
2026-01-29 12:58:28 -0300

    feat: implement hooks for toast and isMobile

---
+++ src/hooks/useToast/types.ts
@@ -0,0 +1,37 @@
+import React from "react";
+import { ToasterProps } from "sonner";
+
+import { ACTION_TYPES } from "./data";
+
+export type ToasterToast = ToasterProps & {
+  id: string;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  action?: React.ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+};
+
+export type ActionType = typeof ACTION_TYPES;
+
+export type Action =
+  | {
+      type: ActionType["ADD_TOAST"];
+      toast: ToasterToast;
+    }
+  | {
+      type: ActionType["UPDATE_TOAST"];
+      toast: Partial<ToasterToast>;
+    }
+  | {
+      type: ActionType["DISMISS_TOAST"];
+      toastId?: ToasterToast["id"];
+    }
+  | {
+      type: ActionType["REMOVE_TOAST"];
+      toastId?: ToasterToast["id"];
+    };
+
+export interface State {
+  toasts: ToasterToast[];
+}
